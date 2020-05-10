0f82379418b811c6fe25c17a5f22377bd3094622
Martin-Sparks <martin.sparks82@gmail.com>
2020-05-10 13:51:07 +0100

    MVP met

--- js/app.js
+++ js/app.js
@@ -1,4 +1,46 @@
-document.addEventListener('DOMContentLoader', () =>{
-    
+document.addEventListener('DOMContentLoaded', () => {
+    const newItemform = document.querySelector('#new-item-form');
+    newItemform.addEventListener('submit', handleNewItemFormSubmit);
 
+    const deleteButton = document.querySelector('#delete');
+    deleteButton.addEventListener('click',handleDeleteClick);
 })
+
+    const handleNewItemFormSubmit = function (event){
+        event.preventDefault();
+
+        console.log(event) 
+
+        const albumListItem = createAlbumList(event.target);
+        const albumList = document.querySelector('#discs');
+        albumList.appendChild(albumListItem);
+
+        event.target.reset();
+    }
+
+    const createAlbumList = function(form) {
+        const albumListItem = document.createElement('li');
+        // albumList.classList.add('album-list');
+        
+
+        const artist = document.createElement('h2');
+            artist.textContent = form.artist.value;
+            albumListItem.appendChild(artist);   
+
+            const album = document.createElement('h3');
+            album.textContent = form.album.value;
+            albumListItem.appendChild(album); 
+
+        const genre = document.createElement('p');
+            genre.textContent = form.category.value;
+            albumListItem.appendChild(genre);   
+            
+            return albumListItem;
+
+    }
+
+    const handleDeleteClick = function (event) {
+        const albumList = document.querySelector('#discs');
+        albumList.innerHTML = '';
+      }
+  
